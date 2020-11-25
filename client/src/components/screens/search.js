22a112fed856e69907c72ba11be9176b4da4ef9a
Soham Bhavsar <soham.bhavsar@hotmail.com>
2020-11-24 20:10:19 -0500

    First commit

--- client/src/components/screens/search.js
+++ client/src/components/screens/search.js
@@ -183,11 +183,11 @@ const Search = () => {
                             <div className="form-container">
                                 <h1>Order Information</h1>
                                 <label id="make">
-                                    <b>Car:</b>
-                                    <label id="car">{name}{car}</label>
+                                    <label className="orderLabel">Car:</label>
+                                    <label className="orderCar" id="car">{name}</label>
                                 </label>
                                 <br />
-                                <label ><b>Start Date:</b></label>
+                                <label className="orderLabel">Start Date:</label>
                                 <DatePicker
                                     className="datepicker"
                                   
@@ -214,7 +214,7 @@ const Search = () => {
                                 <h1>Order Information</h1>
                                 <label id="make">
                                     <b>Car:</b>
-                                    <label id="car">{name}{car}</label>
+                                    <label id="car">{name}</label>
                                 </label>
                                 <br />
                                 <label ><b>End Date:</b></label>
